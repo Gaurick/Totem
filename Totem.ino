8303933d12280439e0058caed20636dec2bac60d
Gaurick <Erik.I.Oasen@gmail.com>
2015-12-18 08:49:27 -0500

    Update Totem.ino
    Buttons wouldn't work as digital inputs on the trinket pin 3, so I changed the buttons all to a single analog input to pin number 4.
    Also cleaned up the initial comment.

--- Totem.ino
+++ Totem.ino
@@ -1,154 +1,125 @@
-/*
-So this was my attempt to create an electronic set of dice.
-It allows 1-9 dice to be rolled at a time and totals the result into one value.
-It can roll d2, d4, d6, d10, d12, d20, and percentiles.
-This requires an arduino micro controller, lipo battery and charger, 3 push buttons, and a seven segment LED display with an i2c converter on it.
-
-Wiring
-i2c seven segment backpack
-c = 2
-d = 0
-+=5v
--=ground
-
-buttons
-button1 = 3
-button2 = 4
-button3 = 1
-
-LiPo backpack 
-bat = BAT+
-g = ground
-5v = USB +
-
-tutorials used to make it work,
-https://learn.adafruit.com/adafruit-led-backpack/0-dot-56-seven-segment-backpack
-https://learn.adafruit.com/adafruit-pro-trinket-lipoly-slash-liion-backpack/pinouts
-
-questions, comments, or advice can be directed at BulletProofHooker@gmail.com
-
-Revision history
-v1.00 - got it running.
-v1.01 - removed rotary encoder, replaced with buttons, set code for trinket and/or trinket pro.
-
-to do
-fix bug when going from a roll to setting the dice to roll (doesn't remember dice value last used, goes to d4).
-add success/failure counter.
-heads or tails (HEAD vs TAIL *should* display on the seven segments.
-add + or - to rolls (for the super lazy…)
-maybe add second seven segment display to increase options of stuff.
+/*dice rolling sketch.
+ * an electronic alternative to rolling dice.
+ * i still prefer to roll dice, but i wanted to try my hand at programming.
+ * 
+ * "rolls" 1-9 dice and can roll d2, d4, d6, d8, d10, d12, d20, and percentile dice.
+ * 
+ * see README for details on wiring, tutorial links, and a better explanation.
+ * 
+ * Revision history
+ * v1.00 - got it running after much trial and tribulations.
+ * v1.01 - removed rotary encoder, replaced with buttons, set code for trinket pro.
+ * v1.02 - after breaking the trinket pro, adjusted to work on trinket.  changed buttons
+ * to function on analog input instead of digital.
+ * 
+ * To do
+ * add success/failure counter (such as rolling for Vampire the Masquerade or Shadowrun).
+ * heads or tails (HEAD vs TAIL *should* work on the seven segment display.)
+ *
 */
- 
-//#include <Wire.h>
+
 #include <TinyWireM.h>
 #include "Adafruit_LEDBackpack.h"
 #include "Adafruit_GFX.h"
-//libraries needed for the seven segment led display.
+//libraries to make it work
 
 Adafruit_7segment matrix = Adafruit_7segment();
-//sets the seven segment display to the name "matrix".
+//sets the 7 segment display to be matrix.[command].
 
-#define numberButton 3
-//pin used for the button to select the number of dice to roll.
-#define sideButton 4
-//pin used for the button to select the number of sides on the dice to roll.
-#define rollButton 1
-//pin used for the button to actually roll the dice.
+#define analogButtons 2
+int analogValue = 0;
+//variables for the buttons.
 
 int diceNumber = 1;
-//variable for the number of dice to "roll".
-int diceSides = 2;
-//variable for the number of sides on the dice to "roll".
 int diceSidesCounter = 1;
-//variable to to track where in the switch case the sides of the roll is, to set the number of die sides.
-int result = 0;
-//variable for the result of the roll.
-int rollArray[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-//array to hold the rolls.
 int rollFlag = 0;
-//flag to keep from infina-rolling.
+//variables to track the button presses.
+
+int diceSides = 0;
+int rollArray[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+int result = 0;
+//variables to do math things to total the rolls.
 
 void setup(){
-  //Serial.begin(9600);
-  
-  pinMode(numberButton, INPUT_PULLUP);
-  //set the rotary button pin to input and use the internal pullup resistor.
-  pinMode(sideButton, INPUT_PULLUP);
-  //set the rotary spinner pin to input and use the internal pullup resistor.
-  pinMode(rollButton, INPUT_PULLUP);
-  //set the pressy button as an input and use the internal pullup resistor.
-  
   matrix.begin(0x71);
   //starts up the seven segment display.
-  
-  randomSeed(analogRead(0));
-  //start the random string of numbers with the noise on an unattached pin 0.
+
+  randomSeed(analogRead(3));
+  //starts the random number.
 }
 
+
 void loop(){
-  if(digitalRead(numberButton) == LOW){
-  //reading and doing the correct action for when the numberButton is pressed.
-    if(diceNumber ==9){
-      diceNumber = 1;
-    }
+  analogValue = analogRead(analogButtons);
+  //check the button
 
-    else{
+  if(analogValue > 685){
+    // the number of dice button is being pressed.
+    //adjust the 685 value based on the results of testing from the analog test.
+    if(diceNumber < 9){
       diceNumber = diceNumber ++;
-    }
-
-  delay(250);
-  }
-    
-  if(digitalRead(sideButton) == LOW){
-  //reading and doing the correct action for when the sideButton is pressed.
-    if(diceSidesCounter == 8){
-      diceSidesCounter = 1;
+      delay(250);
     }
 
     else{
-      diceSidesCounter = diceSidesCounter ++;
+      diceNumber = 1;
+      delay(250);
     }
- 
-    delay(250);
+  }
+
+  else if(analogValue > 628){
+    //the number of sides of dice button is being pressed.
+    //adjust the 628 value based on the results of the analog test.
+    if(diceSidesCounter < 8){
+      diceSidesCounter = diceSidesCounter ++;
+      delay(250);
     }
 
-  if(digitalRead(rollButton) == LOW){  
-  //finally what happens when the roll button is pressed.
-    rollFlag = 1;
-    for(int i = 0; i < 10; i  ++){
-      rollArray[i] = 0;
-      //clear the roll array.
+    else{
+      diceSidesCounter = 1;
+      delay(250);
     }
-    
+  }
+
+  else if(analogValue > 594){
+    //the button to roll was pressed.
+    //adjust the 594 value based on the results of the analog test.
+    //this stuff sets the flag to roll the dice, then clears the results of the last roll.
+    rollFlag = 1;
+    for(int i = 0; i < 10; i ++){
+      rollArray[i] = 0;
+    }
+
     result = 0;
     diceSidesCounter = 0;
     delay(250);
   }
   
   sort();
-  rollFlag = 0; 
+  rollFlag = 0;
 }
 
 void sort(){
+  //sort the results, assign the number of sides on the die (or dice) to be rolled.
   switch(diceSidesCounter){
     case 0:
     roll();
     break;
-    //case to display the "roll"
-    
+    //case to show the roll.
+
     case 1:
     diceSides = 2;
     show();
     break;
-    //d2
-    
+    //d2 (or a coin...)
+
     case 2:
     diceSides = 4;
     show();
     break;
     //d4
-    
-    case 3:
+   
+   case 3:
     diceSides = 6;
     show();
     break;
@@ -171,13 +142,13 @@ void sort(){
     show();
     break;
     //d12
-    
+
     case 7:
     diceSides = 20;
     show();
     break;
-    //famous d20
-    
+    //d20
+
     case 8:
     diceSides = 99;
     show();
@@ -187,36 +158,31 @@ void sort(){
 }
 
 void roll(){
+  //function to roll the dice and total said roll(s).
   if(rollFlag == 1){
     for(int h = 0; h < (diceNumber); h ++){
       rollArray[h] = random(1, (diceSides + 1));
     }
-    //the actual "rolling" of the "dice".  
- 
-    for(int j = 0; j < (diceNumber); j ++){
-      result = result + rollArray[j];
+
+    for(int j = 0; j < (diceNumber), j ++;){
+      result = rollArray[j] + result;
     }
-    //totaling the rolls into one number.
-    
+  }
+  
     matrix.print(result);
     matrix.writeDisplay();
-    //print the results on the seven segments.
-  }
+    //show the total roll.
 }
 
 void show(){
-  matrix.writeDigitNum(0, (diceNumber));
-  //writes the number of dice to be rolled.
+  //show what is about to be rolled (number of dice and number of sides on said dice).
+  matrix.writeDigitNum(0, diceNumber);
   matrix.writeDigitRaw(1, 64);
-  //writes the dash to the seven segment display.
-  matrix.writeDigitNum(3, (diceSides / 10) % 10);
-  //writes the 10's place of the number of sides on the dice to be rolled.
-  matrix.writeDigitNum(4, (diceSides % 10));
-  //writes the 1's place of the number of sides on the dice to be rolled.
+  //dash
+  matrix.writeDigitNum(3, (diceSides / 10)% 10);
+  matrix.writeDigitNum(4, (diceSides %10));
   matrix.writeDisplay();
-  //actually sends the above stuff to be displayed.
 }
-
 
 
 
