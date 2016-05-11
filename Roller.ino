e5a8de561c787237440453e8e5521d90b219ba05
Erik Oasen <Erik.I.Oasen@gmail.com>
2016-05-11 15:10:50 -0400

    Update Roller.ino
    put in the option to have multiple rolls of the dice.
    need to test to see if the arduino brain can handle it.

--- Roller.ino
+++ Roller.ino
@@ -1,5 +1,6 @@
 int sides = 20;
 int dice = 2;
+int rolls = 5;
 
 int hold [3] = {0, 0};
 
@@ -10,13 +11,17 @@ void setup() {
 
 void loop() {
   Serial.println("start.");
-
-  for(int x = 0; x < dice; x++){
-    hold [0] = random(1, sides);
-    Serial.print("roll "); Serial.print(x); Serial.print(" ="); Serial.println(hold [0]);
-    hold [1] = hold [0] + hold [1];
+  
+  for(int y = 0; y < rolls; y++){
+    Serial.print("roll number "); Serial.print(y);
+    for(int x = 0; x < dice; x++){
+      hold [0] = random(1, sides);
+      Serial.print("die number "); Serial.print(x); Serial.print(" ="); Serial.println(hold [0]);
+      hold [1] = hold [0] + hold [1];
+    }
+    Serial.print("total= "); Serial.println(hold[1]);
+    hold [1] = 0;
+    delay(10000);
+    Serial.println("end.");
   }
-  Serial.print("total= "); Serial.println(hold[1]);
-  hold [1] = 0;
-  Serial.println("end.");
 }
