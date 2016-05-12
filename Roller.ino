66935197882b626887c68d235f437d3d4ffac333
Erik Oasen <Erik.I.Oasen@gmail.com>
2016-05-12 09:02:10 -0400

    Update Roller.ino
    added in a bunch of for loops.
    now it can roll multiples of the same dice and rolls them a bunch of times.
    totals the rolls.
    puts the total rolls into an array.
    then sorts through the roll result numbers and shows how many rolls you got.
    
    so you can see that rolling 100 2d6's you got 1 roll of 2, 5 rolls of 3, 20 rolls of 4, and so on.
    
    need to comment it up (there are none) and maybe just have it spit out csv's...

--- Roller.ino
+++ Roller.ino
@@ -1,8 +1,10 @@
 int sides = 20;
-int dice = 2;
-int rolls = 5;
+int dice = 5;
+int rolls = 100;
 
 int hold [3] = {0, 0};
+int totals [101];
+int numbers [101];
 
 void setup() {
   Serial.begin(9600);
@@ -11,17 +13,50 @@ void setup() {
 
 void loop() {
   Serial.println("start.");
+  Serial.print("rolling "); Serial.print(dice); Serial.print("d"); Serial.println(sides);
+  Serial.println();
+  for(int a = 0; a < rolls; a++){
+    totals [a] = 0;
+    numbers [a] = 0;
+  }
   
-  for(int y = 0; y < rolls; y++){
-    Serial.print("roll number "); Serial.print(y);
-    for(int x = 0; x < dice; x++){
-      hold [0] = random(1, sides);
-      Serial.print("die number "); Serial.print(x); Serial.print(" ="); Serial.println(hold [0]);
+  for(int b = 0; b < rolls; b++){
+    Serial.print("roll number "); Serial.println(b);
+    for(int c = 0; c < dice; c++){
+      hold [0] = random(1, (sides + 1));
+      Serial.print("die number "); Serial.print(c); Serial.print(" ="); Serial.println(hold [0]);
       hold [1] = hold [0] + hold [1];
     }
-    Serial.print("total= "); Serial.println(hold[1]);
+    Serial.print("total= "); Serial.println(hold[1]); Serial.println();
+    totals [b] = hold [1];
     hold [1] = 0;
-    delay(10000);
-    Serial.println("end.");
   }
+  
+  Serial.print("results of "); 
+  Serial.print(rolls); Serial.print(" rolls of "); Serial.print(dice); Serial.print("d"); Serial.println(sides);
+  Serial.print("[");
+  for(int d = 0; d < rolls; d++){
+    Serial.print(totals [d]); Serial.print(", ");
+  }
+  Serial.println("]");
+
+  for(int e = 0; e < rolls; e++){
+    for(int f = 0; f < ((sides * dice) + 1); f++){
+      if(totals [e] == f){
+        numbers [f] ++;
+      }
+    }
+  }
+
+  Serial.print("totals of numbers rolled with "); 
+  Serial.print(rolls); Serial.print(" rolls of "); Serial.print(dice); Serial.print("d"); Serial.println(sides);
+  Serial.print("{");
+  for(int g = dice; g < ((sides * dice) + 1); g++){
+    Serial.print(g); Serial.print("=");
+    Serial.print(numbers [g]); Serial.print(", ");
+  }
+  Serial.println("}");
+  
+  delay(10000);
+  Serial.println("end."); Serial.println();
 }
