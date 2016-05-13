e48cd521a1eb560b0bdb3a7d9d048dbacac3bfbf
Erik Oasen <Erik.I.Oasen@gmail.com>
2016-05-13 14:46:51 -0400

    Update Roller.ino
    works, if there was more ram.
    need to try and try and free up some.
    also need to put in those comments.

--- Roller.ino
+++ Roller.ino
@@ -1,12 +1,13 @@
 //250 max range (number of sides * number of dice such as 5d100's, 12d20's, 62d4's.)
-int sides = 10;
-int dice = 1;
-int rolls = 100;
+int sides;
+int dice;
+int rolls = 50;
 //100 max number of rolls.
+int err = 0;
 
 int hold [3] = {0, 0};
-int totals [101];
-int numbers [501];
+int totals [51];
+int numbers [401];
 
 void setup() {
   Serial.begin(115200);
@@ -14,9 +15,27 @@ void setup() {
 }
 
 void loop() {
-  Serial.println("start.");
+  sideSelect();
 
-  Serial.println("How many sides are on the die or dice you want to roll?");
+  if(err == 1){
+    sideSelect();
+  }
+  diceSelect();
+
+  if(err == 1){
+    diceSelect();
+  }
+  rolling();
+
+  if(err == 1){
+    rolling();
+  }
+  resultsSelect();
+}
+
+void sideSelect(){
+  err = 0;
+  Serial.println("How many sides are on the dice you want to roll?");
   Serial.println("1 for d2 (coin)");
   Serial.println("2 for d4");
   Serial.println("3 for d6");
@@ -27,64 +46,53 @@ void loop() {
   Serial.println("8 for d100 (percentile)");
 
   while(!Serial.available());
-  char x = Serial.read();
+  int x = Serial.read();
 
-  switch(x){
-    case '1':{
-      sides = 2;
-      Serial.println("d2 or a coin");
-      break;
-    }
-
-    case '2':{
-      sides = 4;
-      Serial.println("d4");
-      break;
-    }
-
-    case '3':{
-      sides = 6;
-      Serial.println("d6");
-      break;    
-    }
-
-    case '4':{
-      sides = 8;
-      Serial.println("d8");
-      break;
-    }
-
-    case '5':{
-      sides = 10;
-      Serial.println("d10");
-      break;
-    }
-
-    case '6':{
-      sides = 12;
-      Serial.println("d12");
-      break;
-    }
-
-    case '7':{
-      sides = 20;
-      Serial.println("d20");
-      break;
-    }
-
-    case '8':{
-      sides = 100;
-      Serial.println("d100 or percentile.");
-      break;
-    }
+  if(x == '1'){
+    sides = 2;
   }
 
-  Serial.println();
+  else if(x == '2'){
+    sides = 4;
+  }
+
+  else if(x == '3'){
+    sides = 6;
+  }
+
+  else if(x == '4'){
+    sides = 8;
+  }
+
+  else if(x == '5'){
+    sides = 10;
+  }
+
+  else if(x == '6'){
+    sides = 12;
+  }
+
+  else if(x == '7'){
+    sides = 20;
+  }
+
+  else if(x == '8'){
+    sides = 100;
+  }
+
+  else{
+    error(1);
+    err = 1;
+  }
+}
+
+void diceSelect(){
+  err = 0;
   Serial.println("How many dice do you want to roll?");
   Serial.print("Maximum is "); 
 
   if(sides == 100){
-    Serial.println("5");
+    Serial.println("4");
   }
 
   else{
@@ -94,9 +102,63 @@ void loop() {
   while(!Serial.available());
   byte y = Serial.read();
   dice = y - 48;
+  if(dice > 9){
+    error(2);
+  }
+
+  else if(dice < 1){
+    error(2);
+  }
+
+  else if(sides == 100){
+    if(dice > 4){
+      error(2);
+    }
+  }
+}
+
+void resultsSelect(){
+  err = 0;
+  Serial.println("Would you rather");
+  Serial.println("(R)esults");
+  Serial.println("(T)otal of numbers rolled");
+  Serial.println("(C)SV");
+
+  while(!Serial.available());
+  char z = Serial.read();
+
+  if(z == 'R'){
+    results();
+  }
   
-  Serial.print("rolling "); Serial.print(dice); Serial.print("d"); Serial.println(sides);
-  Serial.println();
+  else if(z == 'r'){
+    results();
+  }
+
+  else if(z == 'T'){
+    total();
+  }
+
+  else if(z == 't'){
+    total();
+  }
+
+  else if(z == 'C'){
+    csv();
+  }
+
+  else if(z == 'c'){
+    csv();
+  }
+
+  else{
+    error(3);
+  }
+}
+
+
+void rolling(){
+  err = 0;
   for(int a = 0; a < rolls; a++){
     totals [a] = 0;
     numbers [a] = 0;
@@ -121,37 +183,9 @@ void loop() {
       }
     }
   }
-
-  Serial.println("Would you rather");
-  Serial.println("(H)uman readable?");
-  Serial.println("or");
-  Serial.println("(C)SV");
-
-  while(!Serial.available());
-  char z = Serial.read();
-
-  if(z == 'H'){
-    human();
-  }
-
-  else if(z == 'C'){
-    csv();
-  }
-
-  else if(z == 'h'){
-    human();
-  }
-
-  else if(z == 'c'){
-    csv();
-  }
-
-  else{
-    Serial.println("Error.");
-  }
 }
 
- void human(){
+void results(){
   Serial.print("results of "); 
   Serial.print(rolls); Serial.print(" rolls of "); Serial.print(dice); Serial.print("d"); Serial.println(sides);
   Serial.print("[");
@@ -159,7 +193,10 @@ void loop() {
     Serial.print(totals [d]); Serial.print(", ");
   }
   Serial.println("]");
+  delay(10000);
+ }
 
+ void total(){
   Serial.print("totals of numbers rolled with "); 
   Serial.print(rolls); Serial.print(" rolls of "); Serial.print(dice); Serial.print("d"); Serial.println(sides);
   Serial.print("{");
@@ -171,7 +208,6 @@ void loop() {
   }
   Serial.println("}");
   delay(10000);
-  Serial.println(); Serial.println("end."); Serial.println();
  }
 
  void csv(){
@@ -182,6 +218,27 @@ void loop() {
   }
   
   delay(10000);
-  Serial.println(); Serial.println("end."); Serial.println(); 
  }
  
+void error(int errorNum){
+  if(errorNum == 1){
+    Serial.println("Please select an option between 1 and 8.");
+    delay(1000);
+  }
+
+  else if(errorNum == 2){
+    if(sides == 100){
+      Serial.println("Please select a value between 1 and 5.");
+      delay(1000);
+    }
+
+    else{
+    Serial.println("Please select an option between 1 and 9.");
+    }
+  }
+
+  else if(errorNum == 3){
+    Serial.println("Please select R, T or C.");
+    delay(1000);
+  }
+}
